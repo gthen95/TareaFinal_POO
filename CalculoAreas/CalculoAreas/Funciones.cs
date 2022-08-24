7ec16867d79a9457997dc3444550f187029f5031
Gerald Then <gthen@mdsoft.com.do>
2022-08-24 12:21:48 -0400

    Menu Seleccionable

---
+++ CalculoAreas/CalculoAreas/Funciones.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoAreas
+{
+    public class Funciones
+    {
+        public static void AlinearTitulos(string title)
+        {
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+            Console.WriteLine(title);
+        }
+
+        public static void RepetirCaracter(string caracter)
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write(caracter);
+
+            }
+            Console.WriteLine();
+        }
+
+
+        public static string ConsoleStringYesOrNot(string dato)
+        {
+            string resultado = string.Empty;
+            while (resultado == string.Empty)
+            {
+                Console.Write(dato);
+                var input = Console.ReadLine();
+
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("\nDebe completar la informacion solicitada...");
+                    resultado = string.Empty;
+                }
+                else if (input.ToUpper() != "S" && input.ToUpper() != "N")
+                {
+                    Console.WriteLine("\nSolo se aceptan los caracteres S o N...");
+                    resultado = string.Empty;
+                }
+                else
+                {
+                    resultado = input;
+                }
+            }
+
+            return resultado;
+        }
+
+        public static double ConsoleDouble(string dato)
+        {
+            double resultado = -1;
+            while (resultado == -1)
+            {
+                Console.Write(dato);
+                var valor = Console.ReadLine();
+
+                double.TryParse(valor, out double n);
+                if (n == 0 || n < 0)
+                {
+                    Console.WriteLine("\nEl valor debe ser numerico y mayor que cero");
+                    resultado = -1;
+                }
+                else
+                {
+                    resultado = n;
+                }
+
+            }
+
+            return resultado;
+        }
+
+    }
+}
--- CalculoAreas/CalculoAreas/Program.cs
+++ CalculoAreas/CalculoAreas/Program.cs
@@ -5,105 +5,148 @@ namespace CalculoAreas
 {
     internal class Program
     {
+        private static string[] OpcionesMenu = new string[]
+        {
+            "1. Calcular Area del Circulo",
+            "2. Calcular Area del Triangulo",
+            "3. Calcular Area del Cuadrado",
+            "4. Salir"
+        };
+
+        private static int x;
+        private static int y;
+
         private static void Main(string[] args)
         {
-            double area;
-            int opcion = 0;
-            string input, input2;
 
+            double area;
             Circulo circulo = new Circulo();
             Triangulo triangulo = new Triangulo();
             Cuadrado cuadrado = new Cuadrado();
 
-            do
+            bool Loop = true;
+            int counter = 0;
+            ConsoleKeyInfo Tecla;
+            bool isFirst = true;
+
+            while (Loop)
             {
-                Console.WriteLine($"------------------------------------------------");
-                Console.WriteLine($"***Menu Principal***");
-                Console.WriteLine($"1. Calcular Area del Circulo");
-                Console.WriteLine($"2. Calcular Area del Triangulo");
-                Console.WriteLine($"3. Calcular Area del Cuadrado");
-                Console.WriteLine($"4. Salir");
-                Console.WriteLine($"------------------------------------------------\n");
-                Console.Write($"Elige tu opcion....: ");
-
-
-                string data = Console.ReadLine();
-                if (data != null)
+                if (!isFirst)
                 {
-                    opcion = int.Parse(data);
+                    string resp = Funciones.ConsoleStringYesOrNot("Deseas realizar otra operacion: {S/N): ");
+
+
+                    if (resp.ToUpper() == "N")
+                    {
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+                    }
+                    Console.Clear();
+                }
+                else
+                {
+                    isFirst = false;
                 }
 
-                switch (opcion)
+                Console.CursorVisible = false;
+                Console.WriteLine("Seleccione una opcion del menu y presione ENTER" + System.Environment.NewLine);
+                x = Console.CursorLeft;
+                y = Console.CursorTop;
+                string Resultado = Menu(OpcionesMenu, counter);
+
+                while ((Tecla=Console.ReadKey(true)).Key != ConsoleKey.Enter)
                 {
-                    case 1:
-                        Console.WriteLine($"\n===Calculando el area del Circulo===");
-                        Console.Write("Escriba el valor del radio: ");
-                        input = Console.ReadLine();
-                        while (!circulo.IsNumeric(input))
-                        {
-                            Console.WriteLine($"Caracter ingresado '{input}' no es un numero. Escriba el valor del radio: ");
-                            input = Console.ReadLine();
-                        }
-                        double r = double.Parse(input);
+                    switch (Tecla.Key)
+                    {
+                        case ConsoleKey.DownArrow:
+                            if (counter == OpcionesMenu.Length - 1) continue;
+                            counter++;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (counter == 0) continue;
+                            counter--;
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.CursorLeft = x;
+                    Console.CursorTop = y;
+
+                    Resultado= Menu(OpcionesMenu,counter);
+
+                }
+
+                switch (counter)
+                {
+                    case 0:
+                        Console.CursorVisible = true;
+                        Funciones.RepetirCaracter("=");
+                        Funciones.AlinearTitulos("|  Calculando el area del Circulo  |");
+                        double r = Funciones.ConsoleDouble("Escriba el valor del radio: ");
                         area = circulo.getArea(r);
                         Console.WriteLine($"El area del circulo es de: {area} \n");
 
                         //Dibujando la Figura Indicada
                         circulo.DibujarFigura("circulo.txt");
                         break;
-                    case 2:
-
-                        Console.WriteLine($"\n===Calculando el area del Triangulo===");
-                        Console.Write("Escriba el valor de la base : ");
-                        input = Console.ReadLine();
-                        while (!triangulo.IsNumeric(input))
-                        {
-                            Console.Write($"Caracter ingresado '{input}' no es un numero. Escriba el valor de la base : ");
-                            input = Console.ReadLine();
-                        }
-                        double b = double.Parse(input);
-                        Console.Write("Escriba el valor de la altura : ");
-                        input2 = Console.ReadLine();
-                        while (!triangulo.IsNumeric(input2))
-                        {
-                            Console.Write($"Caracter ingresado '{input2}' no es un numero. Escriba el valor de la altura : ");
-                            input2 = Console.ReadLine();
-                        }
-                        double h = double.Parse(input2);
+                    case 1:
+                        Console.CursorVisible = true;
+                        Funciones.RepetirCaracter("=");
+                        Funciones.AlinearTitulos("|  Calculando el area del Triangulo  |");
+                        double b = Funciones.ConsoleDouble("Escriba el valor de la base : ");
+                        double h = Funciones.ConsoleDouble("Escriba el valor de la altura : ");
                         area = triangulo.getArea(b, h);
                         Console.WriteLine($"El area del triangulo es de: {area} \n");
 
                         //Dibujando la Figura Indicada
                         triangulo.DibujarFigura("triangulo.txt");
                         break;
-                    case 3:
-                        Console.WriteLine($"\n===Calculando el area del Cuadrado===");
-                        Console.Write("Escriba el valor de uno de los lados : ");
-                        input = Console.ReadLine();
-                        while (!cuadrado.IsNumeric(input))
-                        {
-                            Console.Write($"Caracter ingresado '{input}' no es un numero. Escriba el valor de uno de los lados : ");
-                            input = Console.ReadLine();
-                        }
-                        double l = double.Parse(input);
+                    case 2:
+                        Console.CursorVisible = true;
+                        Funciones.RepetirCaracter("=");
+                        Funciones.AlinearTitulos("|  Calculando el area del Cuadrado  |");
+                        double l = Funciones.ConsoleDouble("Escriba el valor de uno de los lados : ");
                         area = cuadrado.getArea(l);
                         Console.WriteLine($"El area del cuadrado es de: {area} \n");
 
                         //Dibujando la Figura Indicada
                         cuadrado.DibujarFigura("cuadrado.txt");
                         break;
-                    case 4:
-                        opcion = 4;
+                    case 3:
+                        Loop = false;
+                        Console.WriteLine("Saliendo del sistema...");
                         break;
-                    default:
-                        Console.WriteLine("Elige una opcion del menu");
-                        break;
+
                 }
             }
-            while (opcion != 4);
+        }
 
+        private static string Menu(string[] items, int opcion)
+        {
+            string SeleccionActual = string.Empty;
+            int destacado = 0;
 
+            Array.ForEach(items, element =>
+            {
+                if(destacado == opcion)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor= ConsoleColor.Red;
+                    Console.WriteLine(" > " + element + " < ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    SeleccionActual = element;
+                }
+                else
+                {
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.CursorLeft = 0;
+                    Console.WriteLine(element);
+                }
+                destacado++;
+            });
 
+            return SeleccionActual;
         }
 
     }
